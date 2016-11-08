e0d44e785ba593d2f30acb183dab1b584327dd9b
Ola Ajibode <ola.ajibode@gmail.com>
2016-11-08 11:12:53 +0000

    start proper scala rampup for scalatest switch

---
+++ genesis.scala
@@ -0,0 +1,17 @@
+class ScalaPdtFactory(x: Int, y: Int) {
+
+val result = x *y
+
+def this(x: Int) = this(x,1)
+
+
+def supplyPdt() : Int = result
+
+}
+
+object  ScalaRunna {
+
+  def main8args:Array([String] ): Unit = {
+    
+  }
+}
