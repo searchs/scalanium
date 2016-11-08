8500f8f82acbcaa7980fe857cda394284a7f5bf5
Ola Ajibode <ola.ajibode@gmail.com>
2016-11-08 11:28:32 +0000

    var and val diff added

--- genesis.scala
+++ genesis.scala
@@ -1,6 +1,6 @@
 class ScalaPdtFactory(x: Int, y: Int) {
-
-val result = x *y
+var instanceVar :String = "I can change you know"
+val result = x *y //I am a readonly guy!   You can't change me!
 
 def this(x: Int) = this(x,1)
 
@@ -9,9 +9,23 @@ def supplyPdt() : Int = result
 
 }
 
+object ScalaClass {
+
+  def mulitply (x: Int, y: Int): Int = {
+    x * y
+  }
+}
+
 object  ScalaRunna {
 
-  def main8args:Array([String] ): Unit = {
-    
+def main( args:Array[String] ): Unit = {
+
+println("Scala runs on JVM!")
+
+val sc = new ScalaClass()
+
+println("Scala simple Class" + sc.mulitply(2,3))
+
+
   }
 }
